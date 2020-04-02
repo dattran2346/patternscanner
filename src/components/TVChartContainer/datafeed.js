f43403e5897d7f6cd63c896f15895767805b96aa
Tran Quang Dat <dattran2346@gmail.com>
2020-04-03 02:17:16 +0700

    Initial commit

---
+++ src/components/TVChartContainer/datafeed.js
@@ -0,0 +1,246 @@
+// import historyProvider from './historyProvider'
+// import realtimeProvider from './realtimeProvider'
+const rp = require('request-promise').defaults({json: true})
+
+// Setup config
+const api_key = 'bp60vd7rh5rcobn2deeg' // THIS IS MY FREE API KEY, SIGNUP AND GET YOUR KEY at https://finnhub.io/register
+// const supportedResolutions = ["1", "5", "15", "30", "60", "D", "W", "M"] // supported resolution by finnhub
+const supportedResolutions = ["D"] // for the sake of this tutorial, we will use only D timeframe
+const config = {
+    supported_resolutions: supportedResolutions
+}; 
+
+
+// history data
+const api_root = 'https://finnhub.io/api/v1'
+const history = {}
+
+
+// realtime data
+const socket_url = 'wss://ws.finnhub.io?token=' + api_key
+const socket = new WebSocket(socket_url)
+var sub;
+var availableSymbols = []
+
+
+// Trading View JS Datafeed
+export default {
+	onReady: cb => {
+		console.log('onReady running')	
+		const qs = {
+			exchange: "binance",
+			token: api_key,
+		}
+		
+		// Get all crypto symbol for binance
+		rp({
+			// uri: 'https://finnhub.io/api/v1/crypto/symbol',
+			uri: api_root + '/crypto/symbol',
+			qs: qs,
+		}).then(data => {
+			availableSymbols = data
+			console.log(availableSymbols)
+		})
+
+		setTimeout(() => cb(config), 0)
+	},
+
+	searchSymbols: (userInput, exchange, symbolType, onResultReadyCallback) => {
+		console.log(userInput)
+		var results = []
+		for (let i in availableSymbols) {
+			let v = availableSymbols[i]
+			if (v.displaySymbol.indexOf(userInput) > -1) {
+				// console.log(v.displaySymbol)
+				let r = {
+					symbol: v.displaySymbol,
+					full_name: v.displaySymbol,
+					description: 'BINANCE:' + v.displaySymbol,
+					exchange: "BINANCE",
+					ticker: v.displaySymbol
+				}
+				results.push(r)
+			}
+		}
+		onResultReadyCallback(results)
+	},
+
+	resolveSymbol: (symbolName, onSymbolResolvedCallback, onResolveErrorCallback) => {
+		// expects a symbolInfo object in response
+		var symbol_stub = {
+			name: symbolName,
+			description: 'Binance '+symbolName,
+			type: 'crypto',
+			session: '24x7',
+			timezone: 'Etc/UTC',
+			ticker: symbolName,
+			exchange: 'BINANCE',
+			minmov: 1,
+			pricescale: 100000000,
+			has_intraday: true,
+			intraday_multipliers: ['1', '60'],
+			supported_resolution:  supportedResolutions,
+			volume_precision: 8,
+			data_status: 'streaming',
+		}
+
+		setTimeout(function() {
+			onSymbolResolvedCallback(symbol_stub)
+			console.log('Resolving that symbol....', symbol_stub)
+		}, 0)
+	},
+
+	getBars: function(symbolInfo, resolution, from, to, onHistoryCallback, onErrorCallback, firstDataRequest) {
+		let symbol = "BINANCE:" + symbolInfo.name.replace('/', '')
+
+		const qs = {
+			symbol: symbol, // "BINANCE:BTCUSDT",
+			resolution: resolution,
+			from: from,
+			to: to,
+			token: api_key,
+		}
+
+		rp({
+			uri: api_root + '/crypto/candle',
+			qs: qs,
+		}).then(data => {
+			if (data['s'] === 'ok') {
+				var bars = data['t'].map(function(t, i) {
+					return {
+						time: t*1000, // trading view need time in milisecond
+						low: data['l'][i],
+						high: data['h'][i],
+						open: data['o'][i],
+						close: data['c'][i]
+					}
+				})
+
+				if (firstDataRequest) {
+					var lastBar = bars[bars.length - 1]
+					history[symbolInfo.name] = {lastBar: lastBar}
+				}
+				return bars
+
+			} else{
+				console.log('Finnhub  API error:',data.Message)
+				return []
+
+			}
+		})
+		// historyProvider.getBars(symbolInfo, resolution, from, to, firstDataRequest)
+		.then(bars => {
+			if (bars.length) {
+				onHistoryCallback(bars, {noData: false})
+			} else {
+				onHistoryCallback(bars, {noData: true})
+			}
+		}).catch(err => {
+			console.log({err})
+			onErrorCallback(err)
+		})
+
+	},
+
+	subscribeBars: (symbolInfo, resolution, onRealtimeCallback, subscribeUID, onResetCacheNeededCallback) => {
+		let symbol = "BINANCE:" + symbolInfo.name.replace('/', '')
+
+		socket.send(JSON.stringify({'type':'subscribe', 'symbol': symbol, resolution: 'D'}))
+
+		const newSub = {
+            symbol,
+            subscribeUID,
+            resolution,
+            symbolInfo,
+            lastBar: history[symbolInfo.name].lastBar,
+            listener: onRealtimeCallback,
+		}
+		sub = newSub
+	},
+
+	unsubscribeBars: subscriberUID => {
+		const symbol = sub.symbol
+        socket.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
+		socket.emit('SubRemove', {subs: [sub.channelString]})
+	},
+}
+
+
+// handle socket event
+socket.onopen = function(event) {
+    console.log('socket open successfully')
+}
+
+
+socket.addEventListener('message', function (event) {
+    //  const _data= e.split('~')
+    const data = JSON.parse(event.data)
+
+    if (data['type'] !== "trade") {
+      return
+    }
+
+    data['data'].forEach(element => {
+        var ticker = {
+            symbol: element['s'],
+            price: element['p'],
+            volume: element['v']
+        }
+
+        if (ticker.time < sub.lastBar.time / 1000) {
+            // disregard the initial catchup snapshot of trades for already closed candles
+            return
+        }
+
+        var _lastBar = updateBar(ticker, sub)
+        // send the most recent bar back to TV's realtimeUpdate callback
+        sub.listener(_lastBar)
+        // update our own record of lastBar
+        sub.lastBar = _lastBar
+    });
+    
+})
+
+// Take a single trade, and subscription record, return updated bar
+function updateBar(data, sub) {
+    var lastBar = sub.lastBar
+    let resolution = sub.resolution
+    if (resolution.includes('D')) {
+        // 1 day in minutes === 1440
+        resolution = 1440
+    } else if (resolution.includes('W')) {
+        // 1 week in minutes === 10080
+        resolution = 10080
+    }
+
+    var coeff = resolution * 60
+    // console.log({coeff})
+    var rounded = Math.floor(data.ts / coeff) * coeff
+    var lastBarSec = lastBar.time / 1000
+    var _lastBar
+    
+    if (rounded > lastBarSec) {
+        // create a new candle, use last close as open **PERSONAL CHOICE**
+        _lastBar = {
+            time: rounded * 1000,
+            open: lastBar.close,
+            high: lastBar.close,
+            low: lastBar.close,
+            close: data.price,
+            volume: data.volume
+        }
+        
+    } else {
+        // update lastBar candle!
+        if (data.price < lastBar.low) {
+            lastBar.low = data.price
+        } else if (data.price > lastBar.high) {
+            lastBar.high = data.price
+        }
+        
+        lastBar.volume += data.volume
+        lastBar.close = data.price
+        _lastBar = lastBar
+    }
+    return _lastBar
+}
---
+++ src/components/TVChartContainer/index.jsx
@@ -0,0 +1,305 @@
+import * as React from 'react';
+import './index.css';
+import { widget } from '../../charting_library/charting_library.min';
+import Datafeed from './datafeed';
+const rp = require('request-promise').defaults({json: true})
+const api_key = 'bp60vd7rh5rcobn2deeg' // THIS IS MY FREE API KEY, SIGNUP AND GET YOUR KEY at https://finnhub.io/register
+
+const api_root = 'https://finnhub.io/api/v1/scan/pattern'
+
+var patternIds = []
+
+function getLanguageFromURL() {
+	const regex = new RegExp('[\\?&]lang=([^&#]*)');
+	const results = regex.exec(window.location.search);
+	return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+export class TVChartContainer extends React.PureComponent {
+	tvWidget = null;
+
+	componentDidMount() {
+		const widgetOptions = {
+			symbol: 'BTC/USDT',
+			// symbol: 'ETC/BTC',
+			datafeed: Datafeed,
+			container_id: 'tv_chart_container',
+			library_path: '/charting_library/',
+			locale: getLanguageFromURL() || 'en',
+			disabled_features: ['use_localstorage_for_settings'],
+			enabled_features: ['study_templates'],
+			charts_storage_url: this.props.chartsStorageUrl,
+			charts_storage_api_version: this.props.chartsStorageApiVersion,
+			fullscreen: false,
+            autosize: true,
+            width: "100%",
+            timezone: "America/New_York",
+            client_id: 'patternscanner',
+            user_id: 'public_user_id',
+            auto_save_delay: 10,
+            theme: 'Light',
+            loading_screen: {backgroundColor: "#222222", foregroundColor: "#229712",},
+		};
+		
+		const tvWidget = new widget(widgetOptions);
+		this.tvWidget = tvWidget;
+		const thisComponent = this;
+
+		tvWidget.onChartReady(() => {
+
+			tvWidget.headerReady().then(() => {
+				const button = tvWidget.createButton();
+				button.setAttribute('title', 'Click to show a notification popup');
+				button.classList.add('apply-common-tooltip');
+				button.addEventListener('click', () => tvWidget.showNoticeDialog({
+					title: 'Notification',
+					body: 'TradingView Charting Library API works correctly',
+					callback: () => {
+						console.log('Noticed!');
+					},
+				}));
+
+				button.innerHTML = 'Check API';
+				thisComponent.getPattern()
+				
+				tvWidget.chart().onIntervalChanged().subscribe(null, function(interval, obj) {
+					console.log('On interval change')
+					thisComponent.getPattern()
+				})
+
+				tvWidget.chart().onSymbolChanged().subscribe(null, function(symbolData) {
+					console.log('Symbol change ' + symbolData)
+					thisComponent.getPattern()
+				})
+			});
+		});
+	}
+
+	getPattern() {
+		let thisComponent = this 
+		thisComponent.removeAllShape()
+
+		let symbol = thisComponent.tvWidget.chart().symbol().replace('/', '')
+		let resolution = thisComponent.tvWidget.chart().resolution()
+		console.log('Get pattern: ' + api_root+'?symbol='+ symbol+ '&resolution='+ resolution+ '&token='+ api_key)
+		const qs = {
+			symbol: symbol,
+			resolution: resolution,
+			token: api_key
+		}
+
+		rp({
+			uri: api_root,
+			qs: qs
+		}).then(data => {
+			let i = 0
+			for (let i in data.points) {
+				let point = data.points[i]
+				thisComponent.drawPattern(thisComponent.tvWidget, point)
+			}
+			console.log('Pattern', data)
+		}).catch(err => {
+			console.log(err)
+		})
+	}
+
+	drawPattern(widget, pattern) {
+		let pname = pattern.patternname.toLowerCase()
+		var patternId = ''
+
+		if (pname == 'triangle' || pname == 'wedge') {
+			let points = [
+				{
+					'time': pattern.atime,
+					'price': pattern.aprice
+				}, 
+				{
+					'time': pattern.btime,
+					'price': pattern.bprice,
+				},
+				{
+					'time': pattern.ctime,
+					'price': pattern.cprice
+				}, 
+				{
+					'time': pattern.dtime,
+					'price': pattern.dprice
+				}
+			]
+			patternId = widget.chart().createMultipointShape(points, {
+				'shape': 'triangle_pattern',
+				disableUndo: true,
+			})
+			patternIds.push(patternId)
+		} else if (pname == 'flag') {
+			// draw flag pole
+			let pole = [
+				{
+					'time': pattern.etime, 
+					'price': pattern.eprice
+				}, 
+				{
+					'time': pattern.atime,
+					'price': pattern.aprice
+				}, 
+			]
+			patternId = widget.chart().createMultipointShape(pole, {
+				'shape': 'trend_line'
+			})
+			patternIds.push(patternId)
+
+			// draw flag 
+			let flag = [
+				{
+					'time': pattern.atime, 
+					'price': pattern.aprice
+				}, 
+				{
+					'time': pattern.ctime,
+					'price': pattern.cprice
+				}, 
+				{
+					'time': pattern.dtime,
+					'price': pattern.dprice
+				}, 
+			]
+			patternId = widget.chart().createMultipointShape(flag, {
+				'shape': 'parallel_channel',
+				disableUndo: true,
+			})
+			patternIds.push(patternId)
+			
+		} else if (pname.indexOf("double") > -1) {
+			// double bottom, double top
+			let points = [
+				{
+					'time': pattern.start_time, 
+					'price': pattern.start_price
+				}, 
+				{
+					'time': pattern.atime, 
+					'price': pattern.aprice
+				}, 
+				{
+					'time': pattern.btime,
+					'price': pattern.bprice
+				}, 
+				{
+					'time': pattern.ctime,
+					'price': pattern.cprice
+				}, 
+				{
+					'time': pattern.end_time,
+					'price': pattern.end_price
+				}
+			]
+			patternId = widget.chart().createMultipointShape(points, {
+				'shape': 'polyline',
+				disableUndo: true,
+				overrides: {"fillBackground": false, "linecolor": "#9528CC", "linewidth": 4}
+			})
+			patternIds.push(patternId)
+		} else if (pname.indexOf("triple") > -1) {
+			// triple top, triple bottom
+			let points = [
+				{
+					'time': pattern.start_time, 
+					'price': pattern.start_price
+				}, 
+				{
+					'time': pattern.atime, 
+					'price': pattern.aprice
+				}, 
+				{
+					'time': pattern.btime,
+					'price': pattern.bprice
+				}, 
+				{
+					'time': pattern.ctime,
+					'price': pattern.cprice
+				}, 
+				{
+					'time': pattern.dtime,
+					'price': pattern.dprice,
+				},
+				{
+					'time': pattern.etime,
+					'price': pattern.eprice
+				},
+				{
+					'time': pattern.end_time,
+					'price': pattern.end_price
+				}
+			]
+			patternId = widget.chart().createMultipointShape(points, {
+				'shape': 'polyline',
+				disableUndo: true,
+				overrides: {"fillBackground": false, "linecolor": "#9528CC", "linewidth": 4}
+			})
+			patternIds.push(patternId)
+		} else if (pname === "head and shoulders") {
+			// head and shoulder pattern
+			let points = [
+				{
+					'time': pattern.start_time, 
+					'price': pattern.start_price
+				}, 
+				{
+					'time': pattern.atime, 
+					'price': pattern.aprice
+				}, 
+				{
+					'time': pattern.btime,
+					'price': pattern.bprice
+				}, 
+				{
+					'time': pattern.ctime,
+					'price': pattern.cprice
+				}, 
+				{
+					'time': pattern.dtime,
+					'price': pattern.dprice,
+				},
+				{
+					'time': pattern.etime,
+					'price': pattern.eprice
+				},
+				{
+					'time': pattern.end_time,
+					'price': pattern.end_price
+				}
+			]
+			patternId = widget.chart().createMultipointShape(points, {
+				shape: 'head_and_shoulders',
+				disableUndo: true,
+			})
+			patternIds.push(patternId)
+
+		} else {
+			console.log(pname, pattern)
+		}
+	}
+
+	removeAllShape() {
+		for (let i in patternIds) {
+			this.tvWidget.chart().removeEntity(patternIds[i].id)
+		}
+		patternIds = []
+	}
+
+	componentWillUnmount() {
+		if (this.tvWidget !== null) {
+			this.tvWidget.remove();
+			this.tvWidget = null;
+		}
+	}
+
+	render() {
+		return (
+			<div
+				id='tv_chart_container'
+				className={ 'TVChartContainer' }
+			/>
+		);
+	}
+}
